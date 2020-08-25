62247f5e3754f46f4c246ad423df4cead9932898
Simon <simon_brandt@gmx.de>
2020-08-25 21:38:14 +0200

    IMPLEMENT basic features

--- src/App.js
+++ src/App.js
@@ -1,26 +1,93 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// Node modules
+import React, { useState } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Components
+import Navbar from './components/Navbar'
+
+// Data
+import initialData from './initialData'
+
+function App () {
+
+    const [ product, setProduct ] = useState('irgendwas')
+    const [ targetGroup, setTargetGroup ] = useState('irgendwen')
+    const [ salesChannel, setSalesChannel ] = useState('irgendeine Plattform')
+
+    const [ lastIdea, setLastIdea ] = useState('')
+
+
+    const getRandomFromArray = (array) => {
+        const randomInt = Math.floor(Math.random() * 1000) % array.length
+        console.log('randomInt:', randomInt)
+        return array[randomInt]
+    }
+
+    const runGenerator = () => {
+        setProduct(getRandomFromArray(initialData.products).name.de)
+        setTargetGroup(getRandomFromArray(initialData.targetGroups).name.de)
+        setSalesChannel(getRandomFromArray(initialData.salesChannels).name.de)
+    }
+
+    const addProduct = (product) => {
+
+    }
+
+    return (
+        <div>
+            <Navbar/>
+            <div className="container ">
+                <div className="row  justify-content-center mb-5" style={ { marginTop: 100 } }>
+                    Wir verkaufen
+                    <span className={ 'highlight' }>{ product }</span>
+                    an
+                    <span className={ 'highlight' }>{ targetGroup }</span>
+                    über
+                    <span className={ 'highlight' }>{ salesChannel }.</span>
+                </div>
+                <div className="row justify-content-center">
+                    <button className="btn btn-primary" onClick={ runGenerator }>Und ab!</button>
+                </div>
+
+                <hr/>
+
+                <div className="card mt-5">
+                    <div className="card-header">Hinzufügen</div>
+                    <div className="card-body">
+                        <div className="row">
+                            <div className="col-12 col-lg-4">
+                                <div className="input-group mb-3">
+                                    <input type="text" className="form-control" placeholder="Produkt"/>
+                                    <div className="input-group-append">
+                                        <button className="btn btn-outline-secondary" type="button">Hinzufügen</button>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-12 col-lg-4">
+                                <div className="input-group mb-3">
+                                    <input type="text" className="form-control" placeholder="Zielgruppe"/>
+                                    <div className="input-group-append">
+                                        <button className="btn btn-outline-secondary" type="button">Hinzufügen</button>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-12 col-lg-4">
+                                <div className="input-group mb-3">
+                                    <input type="text" className="form-control" placeholder="Plattform"/>
+                                    <div className="input-group-append">
+                                        <button className="btn btn-outline-secondary" type="button">Hinzufügen</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="card mt-5">
+                    <div className="card-header">log</div>
+                    <div className="card-body">...</div>
+                </div>
+            </div>
+        </div>
+    )
 }
 
-export default App;
+export default App
---
+++ src/initialData.js
@@ -0,0 +1,62 @@
+export default {
+    "targetGroups": [
+        {
+            "name": {
+                "en": "retired people",
+                "de": "Rentner"
+            },
+            "parentGroupId": null
+        },
+        {
+            "name": {
+                "en": "students",
+                "de": "Studenten"
+            },
+            "parentGroupId": null
+        }
+    ],
+    "products": [
+        {
+            "name": {
+                "en": "insurance",
+                "de": "Versicherungen"
+            },
+            "parentGroupId": null
+        },
+        {
+            "name": {
+                "en": "ebooks",
+                "de": "E-Books"
+            },
+            "parentGroupId": null
+        }
+    ],
+    "salesChannels": [
+        {
+            "name": {
+                "en": "Instagram",
+                "de": "Instagram"
+            },
+            "parentGroupId": null
+        },
+        {
+            "name": {
+                "en": "Facebook",
+                "de": "Facebook"
+            },
+            "parentGroupId": null
+        }
+    ],
+    "ideas": [
+        {
+            "combination": {
+                "product": null,
+                "targetGroup": null,
+                "salesChannel": null
+            },
+            "combinationIdHash": null,
+            "positiveVotes": 0,
+            "negativeVotes": 0
+        }
+    ]
+}
